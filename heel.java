f97720e91997f9395b56c93c565cac8b54345690
CarlosAlsoudi <alsoudi9288@gmail.com>
2023-11-28 21:20:37 -0500

    V4

--- heel.java
+++ heel.java
@@ -3,12 +3,7 @@ package review2;
 public class heel {
     public static void main(String[] args) {
 
-        System.out.println(" Hello World!");
-        System.out.println(" Hello World11!");
-        System.out.println(" Hello World22!");
-        System.out.println(" Hello World33!");
-        System.out.println(" Hello World44!");
-        System.out.println(" Hello World55!");
+        System.out.println("I'm sleepy");
 
     }
 }

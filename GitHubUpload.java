fafd37d7b0cc05c603d6105be52c32b117bc2c7d
CarlosAlsoudi <alsoudi9288@gmail.com>
2023-11-30 09:20:41 -0500

    git test

---
+++ GitHubUpload.java
@@ -0,0 +1,10 @@
+package review2;
+
+public class GitHubUpload {
+
+        public static void main(String[] args) {
+
+            System.out.println("I'm sleepy");
+
+        }
+    }
--- heel.java
+++
@@ -1,9 +0,0 @@
-package review2;
-
-public class heel {
-    public static void main(String[] args) {
-
-        System.out.println("I'm sleepy");
-
-    }
-}
